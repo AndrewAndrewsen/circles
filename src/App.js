ffc52bc3e748a3ea6e46d136bd6ae76068d276a1
Anders <mail@andersandersson.net>
2020-02-09 19:35:05 +0100

    Add circles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import styles from './Circles.module.css';
+
+
+
+const App = () => {
+
+  const [circles, setCirlces] = useState(null);
+
+  const draw = () => {
+    let c = React.createElement("div");
+
+    for (let i = 0; i < 100; i++) {
+      let asd = React.createElement("div", { className: styles.circle }, c);
+      
+      c = asd;
+    }
+
+
+
+    
+    setCirlces(c);
+  };
+
+  useEffect(() => {
+    draw();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className={styles.Circles}>
+
+        {
+          circles
+        }
+        
+
+      </div>
+
     </div>
   );
 }
